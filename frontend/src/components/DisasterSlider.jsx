0ce74f8be04c4887ecf2d9218e80ac83037e3f0a
Sanskar Omar <heysanskar0.0@gmail.com>
2023-03-25 03:41:59 +0530

    Added DisasterSlider, Divider, Quotes Components

---
+++ frontend/src/components/DisasterSlider.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Carousel } from "flowbite-react";
+import DroughtImage from "../assets/images/Disasters/Drought.jpg";
+import FloodImage from "../assets/images/Disasters/Flood.jpg";
+import EarthImage from "../assets/images/Disasters/Earthquake.jpg";
+
+const DisasterSlider = () => {
+  // let disasterImages = [];
+  return (
+    <>
+      <div className="h-[21rem] sm:h-[24rem] xl:h-[90vh] my-[72px]">
+        <Carousel slideInterval={2500}>
+          <img src={DroughtImage} alt="Drought" />
+          <img src={FloodImage} alt="Flood" />
+          <img src={EarthImage} alt="Earthquake" />
+        </Carousel>
+      </div>
+    </>
+  );
+};
+
+export default DisasterSlider;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,65 +1,20 @@
 import React from "react";
 import Nav from "../components/Nav";
+import DisasterSlider from "../components/DisasterSlider";
 import Stats from "../components/Stats";
+import Quotes from "../components/Quotes";
+import Divider from "../components/Divider";
 import AboutUs from "../components/AboutUs";
 import Footer from "../components/_Footer";
-import { Card, Carousel } from "flowbite-react";
 
 const Home = () => {
   return (
     <>
       <Nav />
+      <DisasterSlider />
       <Stats />
-      <Carousel
-        indicators={false}
-        slideInterval={15000}
-        className="px-24 my-8 border-none h-28"
-        leftControl=""
-        rightControl=""
-      >
-        <Card className="border-none">
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            <span className="text-5xl">“</span>
-            The power of one, if fearless and focused, is formidable, but the
-            power of many working together is better.
-            <span className="text-5xl">”</span>
-          </h5>
-          <p className="font-normal text-right text-gray-700 dark:text-gray-400">
-            - Gloria Macapagal Arroyo
-          </p>
-        </Card>
-        <Card className="border-none">
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            <span className="text-5xl">“</span>
-            If you can't feed a hundred people, then feed just one."
-            <span className="text-5xl">”</span>
-          </h5>
-          <p className="font-normal text-right text-gray-700 dark:text-gray-400">
-            - Mother Teresa
-          </p>
-        </Card>
-        <Card className="border-none">
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            <span className="text-5xl">“</span>
-            We make a living by what we get, but we make a life by what we
-            give."
-            <span className="text-5xl">”</span>
-          </h5>
-          <p className="font-normal text-right text-gray-700 dark:text-gray-400">
-            - Winston Churchill
-          </p>
-        </Card>
-        <Card className="border-none">
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            <span className="text-5xl">“</span>A single act of kindness throws
-            out roots in all directions, and the roots spring up and make new
-            trees."<span className="text-5xl">”</span>
-          </h5>
-          <p className="font-normal text-right text-gray-700 dark:text-gray-400">
-            - Amelia Earhart
-          </p>
-        </Card>
-      </Carousel>
+      <Quotes />
+      <Divider text="About Us" />
       <AboutUs />
       <Footer />
     </>
