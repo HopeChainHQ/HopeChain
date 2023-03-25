0588144810bda2aaecbcdb7cba977ec217665a9a
Priyav Kaneria <priyavkaneria@gmail.com>
2023-03-25 15:40:19 +0530

    Integration of Disasters and frontend improvements

--- frontend/src/components/DisasterSlider.jsx
+++ frontend/src/components/DisasterSlider.jsx
@@ -1,22 +1,116 @@
-import React from "react";
-import { Carousel } from "flowbite-react";
-import DroughtImage from "../assets/images/Disasters/Drought.jpg";
-import FloodImage from "../assets/images/Disasters/Flood.jpg";
-import EarthImage from "../assets/images/Disasters/Earthquake.jpg";
+import React, { useEffect, useState } from "react"
+import { Carousel } from "flowbite-react"
+import DroughtImage from "../assets/images/Disasters/Drought.jpg"
+import FloodImage from "../assets/images/Disasters/Flood.jpg"
+import EarthImage from "../assets/images/Disasters/Earthquake.jpg"
+
+import DisasterArtifact from "../contracts/DisasterDonate.json"
+import contractAddress from "../contracts/contract-address.json"
+
+import {
+	// etherToWei,
+	getAllDisasterData,
+} from "../utils"
+import { useContract, useContractRead } from "../hooks"
+import { useNavigate } from "react-router-dom"
 
 const DisasterSlider = () => {
-  // let disasterImages = [];
-  return (
-    <>
-      <div className="h-[21rem] sm:h-[24rem] xl:h-[90vh] my-[0px] md:my-[72px]">
-        <Carousel slideInterval={2500}>
-          <img src={DroughtImage} alt="Drought" />
-          <img src={FloodImage} alt="Flood" />
-          <img src={EarthImage} alt="Earthquake" />
-        </Carousel>
-      </div>
-    </>
-  );
-};
+	const navigator = useNavigate()
+	// The info of the token (i.e. It's Name and symbol)
+	const [disasters, setDisasters] = useState()
 
-export default DisasterSlider;
+	const { contract, initializeContract } = useContract()
+	const { readMethod } = useContractRead()
+
+	useEffect(() => {
+		_initialize()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+	const _initialize = async () => {
+		// This method initializes the dapp
+
+		// Fetching the Disasters are specific to this
+		// sample project, but you can reuse the same initialization pattern.
+		_intializeEthers()
+	}
+
+	const _intializeEthers = () => {
+		initializeContract({
+			address: contractAddress.DisasterDonate,
+			abi: DisasterArtifact.abi,
+		})
+	}
+
+	const _updateDisasters = async () => {
+		const disasters = await readMethod(contract, getAllDisasterData)
+		setDisasters(disasters)
+	}
+
+	useEffect(() => {
+		if (contract) {
+			_updateDisasters()
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [contract])
+
+	const handleImageSrc = (disaster) => {
+		switch (disaster) {
+			case "drought":
+				return DroughtImage
+			case "hurricane":
+				return FloodImage
+			case "earthquake":
+				return EarthImage
+			default:
+				return DroughtImage
+		}
+	}
+
+	return (
+		<>
+			<div className='h-[21rem] sm:h-[24rem] xl:h-[90vh] my-[0px] md:my-[72px]'>
+				<Carousel slideInterval={2500}>
+					{disasters &&
+						disasters.map((disaster, index) => (
+							<div
+								key={index}
+								className='h-full w-full'
+								onClick={() => {
+									navigator(`/disaster/${index}`)
+								}}>
+								<img
+									className='w-full h-full'
+									src={handleImageSrc(disaster[1])}
+									alt={disaster[1]}
+								/>
+								<div className='absolute bottom-5 left-5 bg-opacity-80 rounded-md bg-white p-4'>
+									{/* Type of disaster */}
+									<p className='text-black text-7xl uppercase'>
+										{disaster[1] === "" ? "Earthquake" : disaster[1]}
+									</p>
+									{/* Severity */}
+									<p className='text-black text-5xl capitalize'>
+										{disaster[0]}
+									</p>
+									{/* Description */}
+									{/* <p className=''>{disaster[2]}</p> */}
+									{/* Affected areas */}
+									<p className='text-black text-2xl'>{disaster[3]}</p>
+									{/* <h5>{disaster[4].toString()}</h5>
+									<h6>{disaster[5].toString()}</h6>
+									<h7>{disaster[6].toString()}</h7>
+									<h8>{disaster[7].toString()}</h8> */}
+									<p className='text-black text-lg'>
+										Click to contribute to relief
+									</p>
+								</div>
+							</div>
+						))}
+				</Carousel>
+			</div>
+		</>
+	)
+}
+
+export default DisasterSlider
--- frontend/src/components/Stats.jsx
+++ frontend/src/components/Stats.jsx
@@ -1,87 +1,94 @@
-import React from "react";
-import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/20/solid";
+import React from "react"
+import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/20/solid"
+import CountUp from "react-countup"
 
 const stats = [
-  {
-    name: "Total Donators",
-    stat: "71,897",
-    previousStat: "70,946",
-    change: "12%",
-    changeType: "increase",
-  },
-  {
-    name: "Top Donations",
-    stat: "5.816 ETH",
-    previousStat: "5.214 ETH",
-    change: "11.54%",
-    changeType: "increase",
-  },
-  {
-    name: "Total Organizations",
-    stat: "24",
-    previousStat: "20",
-    change: "4",
-    changeType: "increase",
-  },
-];
+	{
+		name: "Total Donators",
+		stat: "71897",
+		previousStat: "70,946",
+		change: "12%",
+		changeType: "increase",
+	},
+	{
+		name: "Top Donations",
+		stat: "5.816 ETH",
+		previousStat: "5.214 ETH",
+		change: "11.54%",
+		changeType: "increase",
+	},
+	{
+		name: "Total Organizations",
+		stat: "24",
+		previousStat: "20",
+		change: "4",
+		changeType: "increase",
+	},
+]
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+	return classes.filter(Boolean).join(" ")
 }
 
 const Stats = () => {
-  return (
-    <div className="px-8 py-2 md:py-4 m-4">
-      <h3 className="text-base font-semibold leading-6 text-gray-900">
-        Last 30 days
-      </h3>
-      <dl className="grid grid-cols-1 mt-5 overflow-hidden bg-white divide-y divide-gray-200 rounded-lg shadow md:grid-cols-3 md:divide-y-0 md:divide-x">
-        {stats.map((item) => (
-          <div key={item.name} className="px-4 py-5 sm:p-6">
-            <dt className="text-base font-normal text-gray-900">{item.name}</dt>
-            <dd className="flex items-baseline justify-between mt-1 md:block lg:flex">
-              <div className="flex items-baseline text-2xl font-semibold text-indigo-600">
-                {item.stat}
-                <span className="ml-2 text-sm font-medium text-gray-500">
-                  from {item.previousStat}
-                </span>
-              </div>
+	return (
+		<div className='px-8 py-2 md:py-4 m-4'>
+			<h3 className='text-base font-semibold leading-6 text-gray-900'>
+				Last 30 days
+			</h3>
+			<dl className='grid grid-cols-1 mt-5 overflow-hidden bg-white divide-y divide-gray-200 rounded-lg shadow md:grid-cols-3 md:divide-y-0 md:divide-x'>
+				{stats.map((item) => (
+					<div key={item.name} className='px-4 py-5 sm:p-6'>
+						<dt className='text-base font-normal text-gray-900'>{item.name}</dt>
+						<dd className='flex items-baseline justify-between mt-1 md:block lg:flex'>
+							<div className='flex items-baseline text-2xl font-semibold text-indigo-600'>
+								<CountUp
+									end={parseFloat(item.stat.replace(" ETH", ""))}
+									duration={2.75}
+									enableScrollSpy={true}
+									separator=','
+									decimals={item.stat.includes(".") ? 4 : 0}
+									suffix={item.stat.includes("ETH") ? " ETH" : ""}
+								/>
+								<span className='ml-2 text-sm font-medium text-gray-500'>
+									from {item.previousStat}
+								</span>
+							</div>
 
-              <div
-                className={classNames(
-                  item.changeType === "increase"
-                    ? "bg-green-100 text-green-800"
-                    : "bg-red-100 text-red-800",
-                  "inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0"
-                )}
-              >
-                {item.changeType === "increase" ? (
-                  <ArrowUpIcon
-                    className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
-                    aria-hidden="true"
-                  />
-                ) : (
-                  <ArrowDownIcon
-                    className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
-                    aria-hidden="true"
-                  />
-                )}
+							<div
+								className={classNames(
+									item.changeType === "increase"
+										? "bg-green-100 text-green-800"
+										: "bg-red-100 text-red-800",
+									"inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0"
+								)}>
+								{item.changeType === "increase" ? (
+									<ArrowUpIcon
+										className='-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500'
+										aria-hidden='true'
+									/>
+								) : (
+									<ArrowDownIcon
+										className='-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500'
+										aria-hidden='true'
+									/>
+								)}
 
-                <span className="sr-only">
-                  {" "}
-                  {item.changeType === "increase"
-                    ? "Increased"
-                    : "Decreased"}{" "}
-                  by{" "}
-                </span>
-                {item.change}
-              </div>
-            </dd>
-          </div>
-        ))}
-      </dl>
-    </div>
-  );
-};
+								<span className='sr-only'>
+									{" "}
+									{item.changeType === "increase"
+										? "Increased"
+										: "Decreased"}{" "}
+									by{" "}
+								</span>
+								{item.change}
+							</div>
+						</dd>
+					</div>
+				))}
+			</dl>
+		</div>
+	)
+}
 
-export default Stats;
+export default Stats
