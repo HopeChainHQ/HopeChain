d012271c34446b59d904b38181a27a319face1aa
Priyav Kaneria <priyavkaneria@gmail.com>
2023-03-26 05:56:42 +0530

    UI bug fixes

--- frontend/src/components/Admin/index.js
+++ frontend/src/components/Admin/index.js
@@ -513,7 +513,7 @@ const Admin = () => {
 				<div className='mt-6 flex items-center justify-end gap-x-6'>
 					<button
 						type='submit'
-						onClick={deleteDisaster}
+						onClick={handleDeleteDisaster}
 						className='inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
 						Delete Disaster
 					</button>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,6 +16,11 @@ const Home = () => {
 			<div className='flex justify-center items-center'>
 				<button
 					type='button'
+					onClick={() =>
+						alert(
+							"Donate to relief fund will be integrated soon. Please go ahead and click on a disaster to donate to the relief fund."
+						)
+					}
 					className='w-full mt-3 inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-Erin text-base font-medium text-black hover:bg-Erin focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-Erin sm:w-auto sm:text-sm'>
 					Donate to Emergency Relief Funds
 				</button>
